192789fa0bdebf942d7af86946407e75a4230e4f
Amith-18 <155166599+Amith-18@users.noreply.github.com>
2024-11-22 19:21:01 +0400

    Add files via upload

---
+++ PopUp/app.js
@@ -0,0 +1,22 @@
+
+let popup   =   document.getElementById('popup');
+let btns =   document.querySelector('.btn');
+
+console.log(addBtn);
+
+function openPopup(){
+    popup.classList.add('open-popup');
+    btns.style.opacity = '0';
+    btns.style.pointerEvents = 'none';
+    btns.style.display = 'none';
+    
+}
+
+function closePopup(){
+    popup.classList.remove('open-popup');
+    btns.style.display = 'block';
+    btns.style.opacity = '1';
+    btns.style.pointerEvents = 'auto';
+    
+
+}
